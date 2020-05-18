ed3e8aca55335977eba7832678d0a571797ca5b1
Stephen Zhang <stephenzhang0713@outlook.com>
2020-05-18 22:00:14 +0800

    类型变量的限定

---
+++ Chapter8/src/pair2/Pair.java
@@ -0,0 +1,38 @@
+package pair2;
+
+/**
+ * @Author: zhanghan
+ * @Date: 2020/5/18 21:23
+ * @Description:
+ */
+public class Pair<T> {
+
+	private T first;
+	private T second;
+
+	public Pair() {
+		first=null;
+		second=null;
+	}
+
+	public Pair(T first, T second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public T getFirst() {
+		return first;
+	}
+
+	public void setFirst(T first) {
+		this.first = first;
+	}
+
+	public T getSecond() {
+		return second;
+	}
+
+	public void setSecond(T second) {
+		this.second = second;
+	}
+}
