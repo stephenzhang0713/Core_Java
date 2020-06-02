b96c41144a97815f8feb9cb3e396b988e4009b6b
zhanghan <stephenzhang0713@outlook.com>
2020-06-02 17:13:23 +0800

    SetTest.java

---
+++ Chapter9/src/set/SetTest.java
@@ -0,0 +1,35 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @Author: zhanghan
+ * @Date: 2020/6/2 16:50
+ * @Description:
+ */
+public class SetTest {
+
+	public static void main(String[] args) {
+		HashSet<String> words = new HashSet<>();
+		long totalTime = 0;
+
+		try (var in = new Scanner(System.in)) {
+			while (in.hasNext()) {
+				String word = in.next();
+				long callTime = System.currentTimeMillis();
+				words.add(word);
+				callTime = System.currentTimeMillis() - callTime;
+				totalTime += callTime;
+			}
+		}
+
+		Iterator<String> iter = words.iterator();
+		for (int i = 1; i < 20 && iter.hasNext(); i++) {
+			System.out.println(iter.next());
+		}
+		System.out.println("...");
+		System.out.println(words.size()+" distinct words."+totalTime+" milliseconds");
+	}
+}
